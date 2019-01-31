b132798b09cdbade3edf04d9b5a89a073a3cf5fa
Rebecca Rupel <becca@Rebecca-Rupels-MacBook.local>
2019-01-30 20:47:13 -0800

    major changes

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 'use strict';
 
 const apiKey = 'xrVbUsjvIHtShE1PeC6Qxe4C18IEpT0UwRC6j3X3'; 
-const searchURL = 'https://api.nasa.gov/DONKI/notifications?startDate=2014-05-01&endDate=2014-05-08&type=all&api_key=xrVbUsjvIHtShE1PeC6Qxe4C18IEpT0UwRC6j3X3';
+const searchURL = 'https://api.nasa.gov/DONKI/';
 
 
 function formatQueryParams(params) {
@@ -11,15 +11,111 @@ function formatQueryParams(params) {
 }
 
 
-function getSpaceWeather(query) {
+function renderFLR(responseJson) {
+  $('#results-list').empty();
+    // iterate through the items array
+    responseJson.forEach(message => {
+      $('#results-list').append(
+        `<li>
+          <p>${message.flrID}</p>
+         </li>`
+     );
+    });
+    //display the results section  
+    $('#results-list').removeClass('hidden');
+};
+
+function renderSEP(responseJson) {
+    console.log(responseJson);
+    $('#results-list').empty();
+    responseJson.forEach(message => {
+      $('#results-list').append(
+        `<li>
+          <p>${message.sepID}</p>
+         </li>`
+      );
+    });
+    $('#results-list').removeClass('hidden');
+};
+
+function renderCME(responseJson) {
+  console.log(responseJson);
+  $('#results-list').empty();
+  responseJson.forEach(message => {
+    $('#results-list').append(
+      `<li>
+        <p>${message.activityID}</p>
+        <p>${message.note}</p>
+       </li>`
+    );
+  });
+  $('#results-list').removeClass('hidden');
+};
+
+function renderIPS(responseJson) {
+  console.log(responseJson);
+  $('#results-list').empty();
+  responseJson.forEach(message => {
+    $('#results-list').append(
+      `<li>
+        <p>${message.activityID}</p>
+       </li>`
+    );
+  });
+  $('#results-list').removeClass('hidden');
+};
+
+function renderGST(responseJson) {
+  console.log(responseJson);
+  $('#results-list').empty();
+  responseJson.forEach(message => {
+    $('#results-list').append(
+      `<li>
+        <p>${message.gstID}</p>
+       </li>`
+    );
+  });
+  $('#results-list').removeClass('hidden');
+};
+
+function renderRBE(responseJson) {
+  console.log(responseJson);
+  $('#results-list').empty();
+  responseJson.forEach(message => {
+    $('#results-list').append(
+      `<li>
+        <p>${message.rbeID}</p>
+       </li>`
+    );
+  });
+  $('#results-list').removeClass('hidden');
+};
+
+function getSpaceWeather(type, startDate, endDate) {
   const params = {
-    key: apiKey,
-    type:  'all, FLR, SEP, CME, IPS, MPC, GST, RBE',
-    startDate: 'yyyy-MM-dd',
-    endDate: 'yyyy-MM-dd', 
+    api_key: apiKey,
+    //type:  type,
+    startDate: startDate,
+    endDate: endDate, 
   };
   const queryString = formatQueryParams(params)
-  const url = searchURL + '?' + queryString;
+  const url = searchURL + type + '?' + queryString;
+
+let renderMethod; 
+
+if (type === 'FLR') {
+  renderMethod = renderFLR; 
+} else if (type === 'SEP') {
+  renderMethod = renderSEP;
+} else if (type === 'CME') {
+  renderMethod = renderCME; 
+} else if (type === 'IPS') {
+renderMethod = renderIPS; 
+} else if (type === 'GST') {
+  renderMethod = renderGST; 
+} else if (type === 'RBE') {
+  renderMethod = renderRBE; 
+}; 
 
   fetch(url)
     .then(response => {
@@ -28,35 +124,37 @@ function getSpaceWeather(query) {
       }
       throw new Error(response.statusText);
     })
-    .then(responseJson => console.log(JSON.stringify(responseJson)))
+    .then(renderMethod)
     .catch(err => {
       $('#js-error-message').text(`Something went wrong: ${err.message}`);
     });
 }
 
-function displayResults(responseJson) {
+
+//function displayResults(responseJson) {
+    //console.log(responseJson);
     // if there are previous results, remove them
-    console.log(responseJson);
-    $('#results-list').empty();
+    //$('#results-list').empty();
     // iterate through the items array
-    responseJson.data.forEach(park => {
-      $('#results-list').append(
-        `<li>
-          <h3>${park.name}</h3>
-          <p>${park.description}</p>
-         </li>`
-      );
-    });
+    //responseJson.forEach(message => {
+      //$('#results-list').append(
+        //`<li>
+          //<p>${message.flrID}</p>
+          //<p>${message.sepID}</p>
+         //</li>`
+     //);
+    //});
     //display the results section  
-    $('#results-list').removeClass('hidden');
-  };
+    //$('#results-list').removeClass('hidden');
+  //};
 
 function watchForm() {
   $('form').submit(event => {
     event.preventDefault();
-    const searchTerm = $('#js-search-term').val();
-    const maxResults = $('#js-max-results').val();
-    getSpaceWeather(searchTerm, maxResults);
+    const type = $('.weather').val();
+    const startDate = $('.startDate').val();
+    const endDate = $('.endDate').val();
+    getSpaceWeather(type, startDate, endDate);
   });
 }
 
