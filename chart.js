78fb6231010d7e15b4e2da9bba26d5fc8e2a4333
Rebecca Rupel <becca@Rebecca-Rupels-MacBook.local>
2019-02-10 15:57:04 -0800

    changes to month and year

--- chart.js
+++ chart.js
@@ -1,6 +1,13 @@
+const dps = {
+	flr: [],
+	sep: [],
+	cme: [],
+	gst: []
+};
+var chart;
 window.onload = function () {
 
-var chart = new CanvasJS.Chart("chartContainer", {
+chart = new CanvasJS.Chart("chartContainer", {
 	animationEnabled: true,
 	title:{
 		text: "Monthly Space Weather"
@@ -25,60 +32,28 @@ var chart = new CanvasJS.Chart("chartContainer", {
 		type: "spline",
 		yValueFormatString: "#0",
 		showInLegend: true,
-		dataPoints: [
-			{ x: new Date(2017,6,24), y: 31 },
-			{ x: new Date(2017,6,25), y: 31 },
-			{ x: new Date(2017,6,26), y: 29 },
-			{ x: new Date(2017,6,27), y: 29 },
-			{ x: new Date(2017,6,28), y: 31 },
-			{ x: new Date(2017,6,29), y: 30 },
-			{ x: new Date(2017,6,30), y: 29 }
-		]
+		dataPoints: dps.flr
 	},
 	{
 		name: "Solar Energetic Particle",
 		type: "spline",
 		yValueFormatString: "#0",
 		showInLegend: true,
-		dataPoints: [
-			{ x: new Date(2017,6,24), y: 20 },
-			{ x: new Date(2017,6,25), y: 20 },
-			{ x: new Date(2017,6,26), y: 25 },
-			{ x: new Date(2017,6,27), y: 25 },
-			{ x: new Date(2017,6,28), y: 25 },
-			{ x: new Date(2017,6,29), y: 25 },
-			{ x: new Date(2017,6,30), y: 25 }
-		]
+		dataPoints: dps.sep
     },
     {
 		name: "Coronal Mass Ejection",
 		type: "spline",
 		yValueFormatString: "#0",
 		showInLegend: true,
-		dataPoints: [
-			{ x: new Date(2017,6,24), y: 20 },
-			{ x: new Date(2017,6,25), y: 20 },
-			{ x: new Date(2017,6,26), y: 25 },
-			{ x: new Date(2017,6,27), y: 25 },
-			{ x: new Date(2017,6,28), y: 25 },
-			{ x: new Date(2017,6,29), y: 25 },
-			{ x: new Date(2017,6,30), y: 25 }
-		]
+		dataPoints: dps.cme
 	},
 	{
 		name: "Geomagnetic Storm",
 		type: "spline",
 		yValueFormatString: "#0",
 		showInLegend: true,
-		dataPoints: [
-			{ x: new Date(2017,6,24), y: 22 },
-			{ x: new Date(2017,6,25), y: 19 },
-			{ x: new Date(2017,6,26), y: 23 },
-			{ x: new Date(2017,6,27), y: 24 },
-			{ x: new Date(2017,6,28), y: 24 },
-			{ x: new Date(2017,6,29), y: 23 },
-			{ x: new Date(2017,6,30), y: 23 }
-		]
+		dataPoints: dps.gst
 	}]
 });
 chart.render();
--- index.js
+++ index.js
@@ -1,53 +1,52 @@
-'use strict';
+"use strict";
 
-const apiKey = 'xrVbUsjvIHtShE1PeC6Qxe4C18IEpT0UwRC6j3X3'; 
-const searchURL = 'https://api.nasa.gov/DONKI/';
-
+const apiKey = "xrVbUsjvIHtShE1PeC6Qxe4C18IEpT0UwRC6j3X3";
+const searchURL = "https://api.nasa.gov/DONKI/";
 
 function formatQueryParams(params) {
-  const queryItems = Object.keys(params)
-    .map(key => `${encodeURIComponent(key)}=${encodeURIComponent(params[key])}`)
-  return queryItems.join('&');
+  const queryItems = Object.keys(params).map(
+    key => `${encodeURIComponent(key)}=${encodeURIComponent(params[key])}`
+  );
+  return queryItems.join("&");
 }
 
-
 function renderFLR(responseJson) {
-  $('#results-list').empty();
-    // iterate through the items array
-    responseJson.forEach(message => {
-      $('#results-list').append(
-        `<li>
+  $("#results-list").empty();
+  // iterate through the items array
+  responseJson.forEach(message => {
+    $("#results-list").append(
+      `<li>
           <p>Flare ID: ${message.flrID}</p>
           <p>Begin Time: ${message.beginTime}</p>
           <p>Class Type: ${message.classType}</p>
           <p>Peak Time: ${message.peakTime}</p>
          </li>`
-     );
-    });
-    //display the results section  
-    $('#results-list').removeClass('hidden');
-};
+    );
+  });
+  //display the results section
+  $("#results-list").removeClass("hidden");
+}
 
 function renderSEP(responseJson) {
-    console.log(responseJson);
-    $('#results-list').empty();
-    responseJson.forEach(message => {
-      $('#results-list').append(
-        `<li>
+  console.log(responseJson);
+  $("#results-list").empty();
+  responseJson.forEach(message => {
+    $("#results-list").append(
+      `<li>
           <p>ID: ${message.sepID}</p>
           <p>Event Time: ${message.eventTime}</p>
           <p>Instruments: ${message.instruments}</p>
          </li>`
-      );
-    });
-    $('#results-list').removeClass('hidden');
-};
+    );
+  });
+  $("#results-list").removeClass("hidden");
+}
 
 function renderCME(responseJson) {
   console.log(responseJson);
-  $('#results-list').empty();
+  $("#results-list").empty();
   responseJson.forEach(message => {
-    $('#results-list').append(
+    $("#results-list").append(
       `<li>
         <p>Activity ID: ${message.activityID}</p>
         <p>Start Time: ${message.startTime}</p>
@@ -55,126 +54,113 @@ function renderCME(responseJson) {
        </li>`
     );
   });
-  $('#results-list').removeClass('hidden');
-};
-
-function renderIPS(responseJson) {
-  console.log(responseJson);
-  $('#results-list').empty();
-  responseJson.forEach(message => {
-    $('#results-list').append(
-      `<li>
-        <p>ID: ${message.activityID}</p>
-        <p>Location: ${message.location}</p>
-        <p>Event Time: ${message.eventTime}</p>
-       </li>`
-    );
-  });
-  $('#results-list').removeClass('hidden');
-};
-
-function renderMPC(responseJson) {
-  console.log(responseJson);
-  $('#results-list').empty();
-  responseJson.forEach(message => {
-    $('#results-list').append(
-      `<li>
-        <p>ID: ${message.mpcID}</p>
-        <p>Event Time: ${message.eventTime}</p>
-       </li>`
-    );
-  });
-  $('#results-list').removeClass('hidden');
-};
+  $("#results-list").removeClass("hidden");
+}
 
 function renderGST(responseJson) {
   console.log(responseJson);
-  $('#results-list').empty();
+  $("#results-list").empty();
   responseJson.forEach(message => {
-    $('#results-list').append(
+    $("#results-list").append(
       `<li>
         <p>ID: ${message.gstID}</p>
         <p>Start Time: ${message.startTime}</p>
        </li>`
     );
   });
-  $('#results-list').removeClass('hidden');
-};
+  $("#results-list").removeClass("hidden");
+}
 
-function renderRBE(responseJson) {
-  console.log(responseJson);
-  $('#results-list').empty();
-  responseJson.forEach(message => {
-    $('#results-list').append(
-      `<li>
-        <p>ID: ${message.rbeID}</p>
-        <p>Event Time: ${message.eventTime}</p>
-        <p>Instruments: ${message.instruments}</p>
-       </li>`
-    );
-  });
-  $('#results-list').removeClass('hidden');
-};
+
+function getDates(){
+// needs to convert month/year to startDate and endDate
+  if (monthOption === "JAN" && year === "2011") {
+    startDate = 01-01-2011
+    endDate = 01-30-2011
+  }
+  else if (month === "FEB" && year === "2011") {
+    startDate = 02-01-2011
+    endDate = 02-28-2011
+  }
+
+}
 
 function getSpaceWeather(startDate, endDate) {
-  const types = ['FLR', 'SEP', 'CME', 'GST'];
+  getDates(); 
+  const types = ["FLR", "SEP", "CME", "GST"];
   const params = {
     api_key: apiKey,
     //type:  type,
     startDate: startDate,
-    endDate: endDate, 
+    endDate: endDate
   };
 
+  const fetches = types.map(type => {
+    const queryString = formatQueryParams(params);
+    const url = searchURL + type + "?" + queryString;
+    return fetch(url).catch((res) => {
+      console.error('There was an error', res);
+    }).then(res => res.json()).catch(() => []);
+  });
 
-const fetches = types.map((type) => {
-  const queryString = formatQueryParams(params);
-  const url = searchURL + type + "?" + queryString;
-  return fetch(url);
-});
-
-Promise.all(fetches).then(renderGraph);
+  Promise.all(fetches).then(renderGraph);
 }
 
+function getDateHistogram(data, /* startTime... */ time) {
+  const histogram = data
+    .reduce(function(acc, event) {
+      return [...acc, event[time]];
+    }, [])
+    .map(time => {
+      // transform 2017-01-01T01:01Z -> 2017-01-01
+      return time.slice(0, 10);
+    })
+    .reduce((counts, time) => {
+      const count = counts[time] || 0;
+      counts[time] = count + 1;
+      return counts;
+    }, {});
+  
+  const series = Object.keys(histogram).map((date) => {
+    return {
+      x: new Date(date),
+      y: histogram[date]
+    };
+  });
+
+  return series;
+}
+
+function resetArray(arr, newArr) {
+  arr.length = 0;
+  arr.push(...newArr);
+}
 
 function renderGraph(dataForEachType) {
-  const flr = dataForEachType[0];
-  const sep = dataForEachType[1];
-  const cme = dataForEachType[2];
-  const gst = dataForEachType[3];
-
-
-  // sum the counts for each day for each type of event
-  //needs to sum up occurences and then return an array
-  // then get the sum of that array 
-
-  var occurencesFLR = data.reduce(function(accumulator, currentValue) {
-    return [...accumulator, ...currentValue.beginTime];
-  })
-
-  console.log(occurencesFLR); 
-
+  const flr = dataForEachType[0]; // beginTime
+  const sep = dataForEachType[1]; // eventTime
+  const cme = dataForEachType[2]; // startTime
+  const gst = dataForEachType[3]; // startTime
 
   // adding data to graph
-  dps.push(flr);
-  dps.push(sep);
-  dps.push(cme);
-  dps.push(gst);
+  resetArray(dps.flr, getDateHistogram(flr, 'beginTime'));
+  resetArray(dps.sep, getDateHistogram(sep, 'eventTime'));
+  resetArray(dps.cme, getDateHistogram(cme, 'startTime'));
+  resetArray(dps.gst, getDateHistogram(gst, 'startTime'));
 
   // rendering new data points
   chart.render();
 }
 
-var renderButton = document.getElementById('renderButton');
+var renderButton = document.getElementById("renderButton");
 //renderButton.addEventListener('click', renderGraph);
 
-
 function watchForm() {
-  $('form').submit(event => {
+  $("form").submit(event => {
     event.preventDefault();
-    const startDate = $('.startDate').val();
-    const endDate = $('.endDate').val();
+    const startDate = $(".startDate").val();
+    const endDate = $(".endDate").val();
     getSpaceWeather(startDate, endDate);
-    renderGraph(dataForEachType); 
   });
 }
 
